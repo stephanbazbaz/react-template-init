c484d6188ed7afc7084201a21b40788244660f78
stephanbazbaz <stephan@pwm.co.il>
2022-10-24 21:31:27 +0300

    added config for template

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,20 @@
 import './App.scss';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 const App = () => {
   return (
-    <div className="App">
-      <h1>APP</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <div className="App">
+            <h1>APP</h1>
+          </div>} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
